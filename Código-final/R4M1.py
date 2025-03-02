3799e57c5a6788333aeda76e3e40a0ab8bee085e
JesusGarnica8684 <lgarciagar@unal.edu.co>
2025-03-02 00:51:24 -0500

    add r4m1

---
+++ Código-final/R4M1.py
@@ -0,0 +1,214 @@
+import random
+import string
+from tabulate import tabulate
+
+#Def string a lista (va a utilizarse tanto para lista con la respuesta como la que ingresar el usuario) 
+def strToList (secuencia : str) -> list:
+    return list(secuencia)
+
+#Def comparar lista- respuesta con lista-usuario (se revisa que las dos sean iguales de largas, sino, se resta 1 punto)
+def compareLengths (listR, listU : list) -> int:
+    score : int = 0
+    if len(listR) == len(listU):
+        score = 0
+        print("LGFG!!! Son del mismo largo (づ ◕‿◕ )づ")
+    elif len(listR) > len(listU):
+        score = -1
+        print("Tch!!! la secuencia es mas larga de lo que ingresaste")
+        print("¿No recuerdas como configuraste la partida? (乛-乛)")
+        print("(-1) punto, por atembao") 
+    else: 
+        score = -1
+        print("Tch!!! la secuencia es mas corta de lo que ingresaste")
+        print("¿No recuerdas como configuraste la partida? (乛-乛)")
+        print("(-1) punto, por atembao") 
+    return score
+        
+#Def comparar mayusculas minúsculas de las listas
+def compareCapnoCap (listR: list, listU : list) -> int:
+    score : int = 0
+    capU : list = []
+    noCapU : list = []
+    for i in listU:
+        if i.isalpha() and i.isupper():  # Verificar si el carácter es alfabético y mayúscula
+            capU.append(i)
+        elif i.isalpha() and i.islower():  # Verificar si el carácter es alfabético y minúscula
+            noCapU.append(i) 
+    for i in listR:
+        if i in capU and i in noCapU:
+            score += 2
+            print (f"Oh! parece que {i} si se encuentra en la lista tanto en mayuscula como minuscula")
+            print ("Un piko por inteliegente ( ˘ ³˘)♥")
+            print ("(+2) puntos")
+        elif i in capU and i not in noCapU:
+            score += 1
+            print (f"Oh! parece que {i} si se encuentra en la lista en mayuscula pero no en minuscula")
+            print ("A la proxima hazlo mejor, ok? (˶ ⚈ Ɛ ⚈ ˵)")
+            print ("(+1) punto")
+        elif i not in capU and i in noCapU:
+            score += 1
+            print (f"Oh! parece que {i} si se encuentra en la lista en minuscula pero no en mayuscula")
+            print ("A la proxima hazlo mejor, ok? (˶ ⚈ Ɛ ⚈ ˵)")
+            print ("(+1) punto")
+        else:  
+            print ("Ah dale, obvio, claro, claro (•ิ _•ิ ).")  
+    return score  
+
+def validar_entrada(usuario_input:str, configuration:dict) -> bool:
+    #Verifica si la entrada del usuario contiene solo caracteres permitidos.
+    allowed_characters = ""
+    if configuration["Data"] == "letras" or configuration["Data"] == "ambos":
+        if configuration["Capital"] == "mayusculas":
+            allowed_characters += string.ascii_uppercase
+        elif configuration["Capital"] == "minusculas":
+            allowed_characters += string.ascii_lowercase
+        elif configuration["Capital"] == "ambas":
+            allowed_characters += string.ascii_letters
+
+    if configuration["Data"] == "numeros" or configuration["Data"] == "ambos":
+        allowed_characters += string.digits
+
+    if all(char in allowed_characters for char in usuario_input) == False : 
+        return "ingresaste mal un caracter, intentalo de nuevo"
+    else:
+        return None
+
+def comparar_existencia(usuario_input:str, org_chain:str) -> list:
+    #Crea una lista de los caracteres que coinciden en ambas listas.
+    return [char for char in set(usuario_input) if char in org_chain]
+
+def comparar_indice_aparicion(usuario_input:str, org_chain:str) -> dict:
+    #Crea un diccionario con los caracteres que coinciden en posicion.
+    indices = {}
+    for char in set(usuario_input):
+        if char in org_chain:
+            indices[char] = [i for i, c in enumerate(org_chain) if c == char]
+    return indices
+
+def configuration_game(configuration:dict): # Pregunta al jugador como quiere jugar
+    # Caracteres
+    while True:
+        data_value = input("    1- Tipo de caracteres (letras, numeros, ambos): ")
+        if data_value in {"letras", "numeros", "ambos"}:
+            configuration["Data"] = data_value
+            break
+        else:
+            print("No esta dentro de las opciones (⩺_⩹)")
+
+    # Capitalización
+    if configuration["Data"] == "letras" or configuration["Data"] == "ambos":
+        while True:
+            capital_value = input("    2- Capitalizacion de letras (mayusculas, minusculas, ambas): ")
+            if capital_value in {"mayusculas", "minusculas", "ambas"}:
+                configuration["Capital"] = capital_value
+                break
+            else:
+                print("No esta dentro de las opciones (⩺_⩹)")
+    else:
+        pass
+    
+    # Repetición
+    while True:
+        repetition_value = input("    3- Repetición de caracteres (si, no): ")
+        if repetition_value == "si" or repetition_value == "no":
+            configuration["Repetition"] = repetition_value
+            break
+        else:
+            print("ಠ_ʖಠ ... si o no")
+    
+    # Cantidad de intentos
+    while True:
+        amount_value = int(input("    4- Cantidad de caracteres (3-10): "))
+        if amount_value >= 3 and amount_value <= 10:
+            configuration["Amount"] = amount_value
+            break
+        else:
+            print("Puedes usar 3, 4, 5, 6 ... ಠ_ʖಠ ... 7, 8, 9, 10")
+
+    # Vidas (intentos para adivinar)
+    while True:
+        lifes_value = input("    5- Intentos (3, 5, 10, infinitos): ")
+        if lifes_value in {3, 5, 10, "infinitos"}:
+            if lifes_value == "infinitos": 
+                lifes_value = float('inf')
+            else: 
+                lifes_value = int(lifes_value)
+            configuration["Lifes"] = lifes_value
+            break
+        else:
+            print("Opcion NO disponible -(`෴´)- ")
+
+    # Imprimir el diccionario en forma de tabla
+    configuration_tab = tabulate(configuration.items(), tablefmt= "grid")
+
+    return configuration_tab
+
+def combinacion_aleatorea(configuration:dict):
+    # Lista para guardar los caracteres con los que se genera la cadena
+    options = [] 
+    mayus = string.ascii_uppercase # Mayusculas
+    minus = string.ascii_lowercase # Minusculas
+    mayus_minus = string.ascii_letters # Ambas
+    nume = string.digits # Numeros
+
+    if configuration["Data"] == "letras" or configuration["Data"] == "ambos":
+        match configuration["Capital"]:
+            case "mayusculas":
+                options.append(mayus)
+            case "minusculas":
+                options.append(minus)
+            case "ambas":
+                options.append(mayus_minus)
+
+    if configuration["Data"] == "numeros" or configuration["Data"] == "ambos":
+        options.append(nume)
+    
+    options_string = "".join(options)
+    lon = configuration["Amount"]
+
+    # Generar cadena aleatoria
+    if configuration["Repetition"] == "no":
+        chain = "".join(random.sample(options_string, lon))
+    else:
+        chain = "".join(random.choices(options_string, k = lon))
+ 
+    # Convertir cadena a lista
+    chain_list = []
+    for elem in chain:
+        chain_list.append(elem)
+
+    return chain_list
+
+if __name__ == "__main__":
+    configuration = {}
+
+    print("\n...Bienvenido a...")
+    print("""┏━━━━━━━━━━━━━━━━━┓\n♡   R4nd.M1n1ng   ♡\n┗━━━━━━━━━━━━━━━━━┛""")
+    print("¿Listo para divertirte? :D")
+    print("Te hare 5 preguntas →")
+
+    start = configuration_game(configuration)
+    print(f"\n{start}")
+
+    org_chain = combinacion_aleatorea(configuration)
+    print(org_chain)
+
+    hiden_chain = "*" * len(org_chain)
+    print(f"\nIntenta adivinar ╰( ͡° ͜ʖ ͡° )つ──☆ {hiden_chain}")
+
+    l_original = strToList(org_chain)
+    user_chain = input("Ingresa tu secuencia de inicio: ")
+    l_user = strToList(user_chain)
+    score : int = compareLengths(l_original, l_user)
+    
+    if score == -1:
+        print ("Revisa bien cuantos caracteres estas jugando con:")
+        print(f"\n{start} \n¿Listo?")
+        user_chain = input("Ingresa la secuencia de inicio de nuevo, esta vez hazlo bien. (乛-乛)")
+        l_user = strToList(user_chain)
+        score : int = -1
+    elif score == -1:
+        print ("GAME OVER, POR FEA")
+    elif score == 0:
+        print ("INICIA PARTIDA （*＾ワ＾*）")
+        score += compareCapnoCap(l_original, l_user)
