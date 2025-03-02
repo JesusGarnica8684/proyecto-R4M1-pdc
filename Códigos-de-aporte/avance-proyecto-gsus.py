2f14de17e3d44220b4f306abf2e459a89435b433
JesusGarnica8684 <lgarciagar@unal.edu.co>
2025-03-01 20:17:02 -0500

    add

--- Códigos-de-aporte/avance-proyecto-gsus.py
+++ Códigos-de-aporte/avance-proyecto-gsus.py
@@ -1,6 +1,17 @@
-from tabulate import tabulate
-configuration = {"uno":"letras",
-                 "dos":"ambas",
-                 "tres":3}
-tabla = tabulate(configuration.items(), headers=["configuracion"], tablefmt="grid")
-print(tabla)
+#Def string a lista (va a utilizarse tanto para lista con la respuesta como la que ingresar el usuario) 
+def strToList (secuencia : str) -> list:
+    return list(secuencia)
+#Def comparar lista- respuesta con lista-usuario (se revisa que las dos sean iguales de largas, sino, se resta 1 punto)
+def compareLengths (listR, listU : list) -> int:
+    score : int = 0
+    if len(listR) == len(listU):
+        score = 1
+        print("LGFG!!! Son del mismo largo (づ ◕‿◕ )づ")
+    elif len(listR) >= len(listU):
+        score = 0
+        print("Tch!!! la secuencia es mas larga de lo que ingresaste")
+        print("¿No recuerdas como configuraste la partida? (乛-乛)")
+    return score
+        
+#Def comparar mayusculas minúsculas de las listas
+def compareCapnoCap (list1, list2 : list) -> int:
